bb8dd761750b78296a5497e37f28e53fa344d0ad
Øyvind Robertsen <oyvind@arcticstyle.net>
2014-09-24 14:43:50 +0200

    add more parsing tests

--- ex3/test/parse_test.clj
+++ ex3/test/parse_test.clj
@@ -5,3 +5,11 @@
 (deftest bin-parse-text-file-from-resources-test
   (testing "that bin-parse can read and properly parse from resources"
     (is (= 1 (bin-parse "resources/board-1-1.txt")))))
+
+(deftest bin-trans-dot-test
+  (testing "that bin-trans can translate . to 0"
+    (is (= 0 (bin-trans '.')))))
+
+(deftest bin-trans-wall-test
+  (testing "that bin-trans can translate # to 1"
+    (is (= 1 (bin-trans '#')))))
