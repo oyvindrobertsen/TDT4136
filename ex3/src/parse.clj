15873abc61e61542fdf3c6e2ce3153d603afdc5e
Øyvind Robertsen <oyvind@arcticstyle.net>
2014-09-24 12:00:20 +0200

    more parsing

--- ex3/src/parse.clj
+++ ex3/src/parse.clj
@@ -1,7 +1,37 @@
-(ns parse)
+(ns parse
+  (:require [clojure.string :refer :all]))
+
+(defrecord Board [start end data])
+
+(defn get-lines
+  "Returns a vector of lines in text file at path."
+  [path]
+  (split-lines (slurp path)))
+
+(defn bin-trans
+  "Mapping function, input format -> binary representation"
+  [chr]
+  '_)
+
+(defn find-character
+  "Finds the first occurence of given character in a grid, returns x y"
+  [grid chr]
+  '_)
+
+(defn find-start
+  "Given an untranslated board grid, returns the coords of the starting point."
+  [grid]
+  (find-character grid "A"))
+
+(defn find-end
+  "Given an untranslated board grid, returns the coords of the ending point."
+  [grid]
+  (find-character grid "B"))
 
 (defn bin-parse
-  "Slurps the given file and parses it to a maze object with info of start/end
-  coordinates and the board represented as a 2D vector"
+  "Slurps the given file and parses it to a board object with info of start/end
+  coordinates and the board represented as a 2D vector. Assumes a non-weighted
+  board."
   [path]
-  (slurp path))
+  (let [grid (get-lines path)]
+    (Board. (find-start grid) (find-end grid) (map bin-trans grid))))
--- ex3/test/parse_test.clj
+++ ex3/test/parse_test.clj
@@ -1,3 +1,7 @@
 (ns parse-test
   (:require [clojure.test :refer :all]
             [parse :refer :all]))
+
+(deftest bin-parse-text-file-from-resources-test
+  (testing "that bin-parse can read and properly parse from resources"
+    (is (= 1 (bin-parse "resources/board-1-1.txt")))))
