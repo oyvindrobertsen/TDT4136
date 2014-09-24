9cacab17b8ee13e51753ee74b28a35713027d4c1
Øyvind Robertsen <oyvind@arcticstyle.net>
2014-09-24 22:52:24 +0200

    implement coord find functions and add tests and test board

--- ex3/src/parse.clj
+++ ex3/src/parse.clj
@@ -11,22 +11,26 @@
 (defn bin-trans
   "Mapping function, input format -> binary representation"
   [chr]
-  '_)
+  (case chr
+    \# 1
+    0))
 
 (defn find-character
   "Finds the first occurence of given character in a grid, returns x y"
   [grid chr]
-  '_)
+  (let [y (.indexOf grid (first (filter (fn [string] (.contains string (str chr))) grid)))
+        x (.indexOf (nth grid y) (str chr))]
+    (vector x y)))
 
 (defn find-start
   "Given an untranslated board grid, returns the coords of the starting point."
   [grid]
-  (find-character grid "A"))
+  (find-character grid \A))
 
 (defn find-end
   "Given an untranslated board grid, returns the coords of the ending point."
   [grid]
-  (find-character grid "B"))
+  (find-character grid \B))
 
 (defn bin-parse
   "Slurps the given file and parses it to a board object with info of start/end
@@ -34,4 +38,8 @@
   board."
   [path]
   (let [grid (get-lines path)]
-    (Board. (find-start grid) (find-end grid) (map bin-trans grid))))
+    (Board. (find-start grid) (find-end grid) (map 
+                                                (fn [string] (map bin-trans string))
+                                                grid))))
+
+
--- ex3/test/parse_test.clj
+++ ex3/test/parse_test.clj
@@ -4,12 +4,30 @@
 
 (deftest bin-parse-text-file-from-resources-test
   (testing "that bin-parse can read and properly parse from resources"
-    (is (= 1 (bin-parse "resources/board-1-1.txt")))))
+    (is (= 1 (bin-parse "resources/test-board.txt")))))
 
 (deftest bin-trans-dot-test
   (testing "that bin-trans can translate . to 0"
-    (is (= 0 (bin-trans '.')))))
+    (is (= 0 (bin-trans \.)))))
 
 (deftest bin-trans-wall-test
   (testing "that bin-trans can translate # to 1"
-    (is (= 1 (bin-trans '#')))))
+    (is (= 1 (bin-trans \#)))))
+
+(deftest bin-trans-start-test
+  (testing "that bin-trans correctly translates start character as 0"
+    (is (= 0 (bin-trans \A)))))
+
+(deftest bin-trans-end-test
+  (testing "that bin-trans correctly translates end character as 0"
+    (is (= 0 (bin-trans \B)))))
+
+
+(deftest find-character-test
+  (testing "that find-character returns a [x y] pair for the given grid and
+           characters"
+    (is (= [1 1] (find-character [
+                                "###"
+                                "#A#"
+                                "###"
+                                ] \A)))))
