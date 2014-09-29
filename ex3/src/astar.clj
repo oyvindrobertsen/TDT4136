da4ee8d59f941fa951ae631b6262b5d82ae3896a
Øyvind Robertsen <oyvind@arcticstyle.net>
2014-09-29 18:38:48 +0200

    add cost estimate test, start implementing neighboring nodes function

--- ex3/src/astar.clj
+++ ex3/src/astar.clj
@@ -4,3 +4,33 @@
   "Calculates the manhattan distance between two points on a 2D grid"
   [[x1 y1] [x2 y2]]
   (+ (Math/abs (- x2 x1)) (Math/abs (- y2 y1))))
+
+
+(defn cost
+ "Calculates the total estimated cost of a solution path from start through 
+ curr to end."
+ [curr start end]
+  (let [g (manhattan-distance start curr)
+        h (manhattan-distance curr end)
+        f (+ g h)]
+    [f g h]))
+
+(defn edges
+  "Returns a list of all , non-closed nodes adjacent to the x, y pair
+  passed"
+  [grid width height closed [x y]]
+  (for [tx (range (- x 1) (+ x 2))
+        ty (range (- y 1) (+ y 2))
+        :when (and (>= tx 0)
+                   (>= ty 0)
+                   (<= tx width)
+                   (<= tx height)
+                   (not= [x y] [tx ty])
+                   (not= (nth (nth grid ty) tx) 1)
+                   (not (contains? closed [tx ty])))]
+    [tx ty]))
+
+(defn search
+  ""
+  []
+  '_)
--- ex3/test/astar_test.clj
+++ ex3/test/astar_test.clj
@@ -9,3 +9,8 @@
 (deftest manhattan-distance-negative-test
   (testing "that manhattan distance calculation handles negative coords"
     (is (= 4 (manhattan-distance [0 0] [-2 -2])))))
+
+(deftest cost-estimate-test
+  (testing "that the cost estimate function correctly calculates the estimate
+           for the given state."
+    (is (= [4 2 2] (cost [1 1] [0 0] [2 2])))))
