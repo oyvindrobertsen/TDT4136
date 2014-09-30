21055a750c0eeb8e143b37d19aa1c7e506274fe3
Øyvind Robertsen <oyvind@arcticstyle.net>
2014-09-30 15:23:51 +0200

    add priority-map dependency

--- ex3/project.clj
+++ ex3/project.clj
@@ -4,6 +4,7 @@
   :license {:name "MIT"
             :url "http://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.priority-map "0.0.5"]
                  [quil "2.2.2"]]
   :main ^:skip-aot core
   :target-path "target/%s"
