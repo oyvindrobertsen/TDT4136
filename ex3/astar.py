4a6c6101fccaec6aa9c166157041d0a6f7078ac9
Øyvind Robertsen <oyvind@arcticstyle.net>
2014-10-03 18:53:54 +0200

    add report

--- ex3/astar.py
+++ ex3/astar.py
@@ -17,11 +17,6 @@ def find_path(grid, draw_all_steps=False, bfs=False, dijkstra=False):
     else:
         heappush(open_nodes, start_node)
 
-        # Code to generate png for each step
-        # if draw_all_steps:
-        # filename = "to_gif_1-1_%04d.png"
-        # filename_count = 1
-
     while open_nodes:
         if bfs:
             node = open_nodes.pop(0)
@@ -76,7 +71,7 @@ if __name__ == "__main__":
     dir_name = os.getcwd()
     for filename in os.listdir(os.path.join(dir_name, 'boards')):
 
-        # Create a Grid object en
+        # Create a Grid object
         filepath = dir_name + '/boards/' + filename
         grid = Grid(filepath)
 
@@ -96,7 +91,7 @@ if __name__ == "__main__":
         else:
             what = 'astar'
 
-
-        new_file = os.path.join(dir_name, "pictures", filename.replace('.txt', '-{}.png'.format(what)))
+        new_file = os.path.join(dir_name, "report/img",
+                filename.replace('.txt', '-{}.png'.format(what)))
 
         draw_map(grid, new_file, path, open_list, closed_list)
