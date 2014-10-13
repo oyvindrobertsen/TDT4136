2176ea7c62e9558f51180591aef4a1491518d36d
Mathias Ose <mathias.ose@gmail.com>
2014-10-13 20:09:34 +0200

    eg egger egg eg

--- ex4/eggses.py
+++ ex4/eggses.py
@@ -1,4 +1,4 @@
-from random import choice
+from random import randrange
 from copy import deepcopy
 
 from numpy.lib.twodim_base import flipud
@@ -30,46 +30,90 @@ MNKs = [(5, 5, 2),
         (8, 8, 1),
         (10, 10, 3)]
 
+def grid_dim(grid):
+    return len(grid[0]), len(grid)
+
+
+def random_coord(M, N):
+    return randrange(M), randrange(N)
+
+
+def new_grid(M, N, val=0):
+    return [[val for _ in range(M)] for _ in range(N)]
+
+
+def random_grid(M, N, K):
+    grid = new_grid(M, N, 0)
+    ts = K * max(M, N)
+    while ts > 0:
+        x, y = random_coord(M, N)
+
+        if grid[y][x] == 1:
+            continue
+
+        grid[y][x] = 1
+        ts -= 1
+
+    return grid
+
+
+def grid_score(k, grid):
+    width, height = grid_dim(grid)
+
+    row_penalties = list(max(0, sum(row) - k) for row in grid)
+    col_penalties = list(max(0, sum(row[j] for row in grid) - k) for j in range(width))
+
+    grid = array(grid)
+    flip = flipud(grid)
+
+    r1 = range(-(width - 1), width)
+    r2 = range(-(height - 1), height)
+
+    dia_penalties = lambda m, dia_i: max(0, int(m.trace(dia_i)) - k)
+
+    di1_penalties = list(dia_penalties(grid, d) for d in r1)
+    di2_penalties = list(dia_penalties(flip, d) for d in r2)
+
+    return sum(row_penalties) + sum(col_penalties) + sum(di1_penalties) + sum(di2_penalties)
+
+
+def random_swap(grid):
+    width, height = grid_dim(grid)
+
+    x1, y1 = random_coord(width, height)
+    x2, y2 = random_coord(width, height)
+
+    while (x1 == x2 and y1 == y2) or grid[y1][x1] == grid[y2][x2]:
+        x2, y2 = random_coord(width, height)
+
+    grid[y1][x1], grid[y2][x2] = grid[y2][x2], grid[y1][x1]
+
+    return grid
+
 
 class Carton:
     def __init__(self, k, grid):
         self.grid = deepcopy(grid)
         self.k = k
-        self.width = len(self.grid[0])
-        self.height = len(self.grid)
+        self.width, self.height = grid_dim(self.grid)
+
+    @property
+    def worst_score(self):
+        one_grid = new_grid(self.width, self.height, 1)
+        return grid_score(self.k, one_grid)
 
     def gen_neighbors(self, n):
         ret = []
         while len(ret) < n:
-            new = self.gen_neighbor()
+            new = Carton(self.k, self.grid)
+            random_swap(new.grid)
             if new not in ret:
                 ret.append(new)
-        return ret
 
-    def gen_neighbor(self):
-        ret = Carton(self.k, self.grid)
-        rnd_x = choice(range(self.width))
-        rnd_y = choice(range(self.height))
-        ret.grid[rnd_y][rnd_x] = int(not ret.grid[rnd_y][rnd_x])
         return ret
 
     def obj_func(self):
-        row_score = sum(map(lambda l: max(0, sum(l) - self.k), self.grid))
-        col_score = sum(max(0, sum(row[j] for row in self.grid) - self.k) for j in range(self.width))
-
-        grid = array(self.grid)
-        flip = flipud(grid)
-
-        r1 = range(-(self.width - 1), self.width)
-        r2 = range(-(self.height - 1), self.height)
-
-        dia_score = lambda grid, dia_i: max(0, grid.trace(dia_i) - min(self.k, len(grid.diagonal(dia_i))))
-
-        di1_score = sum(dia_score(grid, d) for d in r1)
-        di2_score = sum(dia_score(flip, d) for d in r2)
-
-        perf = self.k * (self.height + self.width + 2 * (self.width + self.height - 1))
-        return (perf - row_score - col_score - di1_score - di2_score) / perf
+        return 1 - (grid_score(self.k, self.grid) / self.worst_score)
 
     def __str__(self):
         ret = ""
@@ -81,10 +125,17 @@ class Carton:
         ret += '\n'
         ret += ' '.join(str(sum(row[j] for row in self.grid)) for j in range(self.width))
         ret += '\n'
-        ret += str(self.obj_func())
+        ret += "Score: {}".format(str(self.obj_func()))
         ret += '\n'
         return ret
 
 
-annealer = SimAnnealer(10, 0.2, 5, 0.99)
-print(annealer.search(Carton(2, ONES)))
+annealer = SimAnnealer(10, 0.2, 5, 0.98)
+
+assert Carton(2, SOLUTION552).obj_func() == 1.0
+assert Carton(2, ONES).obj_func() == 0.0
+
+carton = Carton(2, random_grid(5, 5, 2))
+print(carton)
+print(annealer.search(carton))
+
